7a24ca90eb86c94de64669e62548321051a22d03
yang lv <370901903@qq.com>
2018-08-17 22:45:26 +0800

    1

---
+++ stream/src/test/java/com/example/demo/RabbitmqHelloApplicationTests.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RabbitmqHelloApplicationTests {
+	
+	@Test
+	public void contextLoads() {
+		
+	}
+	
+	@Test
+	public void hello() {
+	}
+
+}
